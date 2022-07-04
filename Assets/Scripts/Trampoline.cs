1bea02f430702957f4cf124c89cc6e9b4ff4d74d
Victor Pereira <vpereirabr@gmail.com>
2022-07-04 13:44:05 +0200

    Trampoline

---
+++ Assets/Scripts/Trampoline.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trampoline : MonoBehaviour
+{
+    [SerializeField]
+    private Animator animator;
+
+    [SerializeField]
+    private float jumpForce;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.name == "Player")
+        {
+            collision.gameObject.transform.SetParent(transform);
+            collision.gameObject.GetComponent<Rigidbody2D>().velocity = (Vector2.up * jumpForce);
+            animator.SetTrigger("jump");
+        } 
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+         if(collision.gameObject.name == "Player")
+        {
+            collision.gameObject.transform.SetParent(null);
+        }
+    }
+}
