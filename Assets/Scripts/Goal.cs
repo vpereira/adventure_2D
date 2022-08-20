0361b3127927f72999711acae18bade6e74efaae
Victor Pereira <vpereirabr@gmail.com>
2022-08-20 13:01:21 +0000

    [Bot] Automated dotnet-format update

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -9,12 +9,12 @@ public class Goal : MonoBehaviour
     Canvas youWon;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.CompareTag("Player"))
+        if (collision.CompareTag("Player"))
         {
             var player = GameObject.Find("Player");
             // TODO
             // We have to control if the door is open or close
-            if(player.GetComponent<KeyCollector>().AreAllKeysFound())
+            if (player.GetComponent<KeyCollector>().AreAllKeysFound())
             {
                 youWon.GetComponent<Canvas>().enabled = true;
                 player.SetActive(false);
--- Assets/Scripts/KeyCollector.cs
+++ Assets/Scripts/KeyCollector.cs
@@ -14,13 +14,13 @@ public class KeyCollector : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     public bool AreAllKeysFound()
     {
